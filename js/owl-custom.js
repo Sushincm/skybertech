df9e4bde269a2ba2987d3a1bbf95dd6547e8d98e
Sushincm <sushinofficial@gmail.com>
2025-12-28 17:37:46 +0530

    responsive header top style and layout changed

--- js/owl-custom.js
+++ js/owl-custom.js
@@ -1,23 +1,45 @@
-$(function () {
-  $(".solutions-carousel").owlCarousel({
-    loop: true,
-    margin: 24,
-    nav: true,
-    dots: false,
-    autoplay: true,
-    responsive: {
-      0: {
-        // mobile: show 1 card
-        items: 1,
+document.addEventListener("DOMContentLoaded", function () {
+  $(function () {
+    $(".solutions-carousel").owlCarousel({
+      loop: true,
+      margin: 24,
+      nav: true,
+      dots: false,
+      autoplay: true,
+      responsive: {
+        0: {
+          // mobile: show 1 card
+          items: 1,
+        },
+        576: {
+          // small tablets: 2 cards
+          items: 2,
+        },
+        992: {
+          // desktops: 4 cards (like your row)
+          items: 4,
+        },
       },
-      576: {
-        // small tablets: 2 cards
-        items: 2,
+    });
+  });
+  //  Header top  owl carousel
+  $(function () {
+    $(".studio-owl").owlCarousel({
+      loop: true,
+      nav: false,
+      dots: false,
+      autoplay: true,
+      autoplayTimeout: 3500,
+      autoplayHoverPause: true,
+
+      stagePadding: 0,
+      margin: 10,
+
+      responsive: {
+        0: { items: 1 },
+        550: { items: 2 },
+        991: { items: 3 },
       },
-      992: {
-        // desktops: 4 cards (like your row)
-        items: 4,
-      },
-    },
+    });
   });
 });
